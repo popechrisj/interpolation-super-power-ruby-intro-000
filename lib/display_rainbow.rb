5245685325c7a9f95ea012780975b460d092f0ec
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-21 19:06:25 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 def display_rainbow(colors)
   for color in colors
     print "#{color[0].upcase}: #{color}"
-    if colors.index(color) < colors.length
+    if colors.index(color) < (colors.length - 1)
       print ", "
     end
   end
