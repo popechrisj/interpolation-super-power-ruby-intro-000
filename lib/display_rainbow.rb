600de08b5aa11370e59629a21180b617c7b48595
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-21 19:15:01 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -11,3 +11,4 @@ def display_rainbow(colors)
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
